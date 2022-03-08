e16c0e2f96e3aa51c1e5dd3b61aea72b0d4bfe51
desmin2102 <101192008+desmin2102@users.noreply.github.com>
2022-03-08 22:14:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'bhan.nyn';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100008442972503';
+const eventName = 'happy 8-3';
+const titleCard = 'Tặng Hân';
+const contentCard = 'chúc bạn Hân 8-3 vui vẻ luôn xinh và bớt khùng hơn nha :))';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'bhan.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
